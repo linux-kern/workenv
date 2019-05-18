266b26720f82620738b19f9796f53438b2e14e3d
Norman <norman.kern@gmx.com>
2019-05-18 13:12:15 +0800

    Init commit

---
+++ git-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "which git" ]; then
+	exit 2
+fi
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.lg log
+git config --global alias.df diff
+
+git config --global user.name='Norman'
+git config --global user.email='norman.kern@gmx.com'
